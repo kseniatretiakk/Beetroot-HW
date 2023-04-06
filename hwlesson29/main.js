c98054068d0f7706c50e3842518955e0f75233d4
Ksenia <kseniatretiakua@gmail.com>
2023-04-06 10:21:16 +0200

    Add HW29

---
+++ hwlesson29/main.js
@@ -0,0 +1,17 @@
+// Greatings
+const userName = prompt("Enter your name.");
+const greatings = prompt('Hi there, '  + userName + '!');
+
+
+// User age
+const userYearOfBirth = prompt("Enter your birth year");
+const currentYear = 2023;
+let userAge = prompt( 'Your age is ' + (currentYear - userYearOfBirth));
+
+
+// Perimeter 
+const sideOfSquare = prompt ("Enter the one side of squre in cm");
+const perimeter = prompt ('The perimeter of square is ' + (sideOfSquare * 4) + ' cm');
+
+
+
